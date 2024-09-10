8a6fb183bd748b7ac84def4bab4a56a081964a9d
Daniel Sallesse <116963571+bmaizena@users.noreply.github.com>
2024-09-10 10:30:58 -0300

    add teste tcc

---
+++ testes/script.js
@@ -0,0 +1,7 @@
+// Script para controlar o menu hambúrguer
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
